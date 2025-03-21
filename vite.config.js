95b98c6e28fee9d1d37ad902d5dda3844681b299
hokou <hokou.shiau@gmail.com>
2025-03-21 13:54:24 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/hex-react-2024-wk4/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/hex-react-2024-wk7/' : '/',
   plugins: [react()],
 })
